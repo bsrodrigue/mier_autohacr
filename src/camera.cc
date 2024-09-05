206b1ef2ac88cdd26ede09f2f1ad6d05671fcb38
BADINI Rachid Rodrigue <bsrodrigue@gmail.com>
2024-09-05 13:17:06 +0000

    refactoring: improve level editor camera movement

--- src/camera.cc
+++ src/camera.cc
@@ -2,22 +2,23 @@
 #include <raymath.h>
 
 void handle_camera_movement(Camera2D *camera) {
-  Vector2 camera_vertical_movement = {0, 0};
-  Vector2 camera_horizontal_movement = {0, 0};
+  Vector2 camera_movement = {0, 0};
 
   if (IsKeyDown(KEY_W)) {
-    camera_vertical_movement = {0, -CAMERA_VELOCITY};
-  } else if (IsKeyDown(KEY_S)) {
-    camera_vertical_movement = {0, CAMERA_VELOCITY};
+    camera_movement.y -= CAMERA_VELOCITY;
+  }
+
+  if (IsKeyDown(KEY_S)) {
+    camera_movement.y += CAMERA_VELOCITY;
   }
 
   if (IsKeyDown(KEY_A)) {
-    camera_horizontal_movement = {-CAMERA_VELOCITY, 0};
-  } else if (IsKeyDown(KEY_D)) {
-    camera_horizontal_movement = {CAMERA_VELOCITY, 0};
+    camera_movement.x -= CAMERA_VELOCITY;
   }
 
-  camera->target =
-      Vector2Add(camera->target, Vector2Add(camera_horizontal_movement,
-                                            camera_vertical_movement));
+  if (IsKeyDown(KEY_D)) {
+    camera_movement.x += CAMERA_VELOCITY;
+  }
+
+  camera->target = Vector2Add(camera->target, camera_movement);
 }
